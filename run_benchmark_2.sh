1d3ccdba9a9c549f29fb5612e261738c4971625e
fedric95 <ricciuti.federico@gmail.com>
2023-05-31 12:11:23 +0200

    Update run_benchmark_2.sh

--- run_benchmark_2.sh
+++ run_benchmark_2.sh
@@ -1,2 +1,2 @@
 docker pull nvcr.io/nvidia/pytorch:23.03-py3
-docker run --gpus all --ipc=host -v /home/ec2-user/SageMaker/benchmark.py:/home/benchmark.py -it nvcr.io/nvidia/pytorch:23.03-py3 sh -c "pip install -U git+https://github.com/qubvel/segmentation_models.pytorch && python /home/benchmark.py"
+docker run --gpus all --ipc=host -v /home/ec2-user/SageMaker/benchmark/benchmark.py:/home/benchmark.py -it nvcr.io/nvidia/pytorch:23.03-py3 sh -c "pip install -U git+https://github.com/qubvel/segmentation_models.pytorch && python /home/benchmark.py"
